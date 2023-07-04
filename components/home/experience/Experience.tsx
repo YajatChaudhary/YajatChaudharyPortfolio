dd412fdcff1da661aceea8d86a9e79125518f6b6
Yajat Chaudhary <chaudharyyajat@gmail.com>
2023-07-04 22:24:36 +0530

    experience page

---
+++ components/home/experience/Experience.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./experience.module.scss";
+const Experience = () => {
+  return <div className={styles.expereince}>Experience</div>;
+};
+
+export default Experience;
