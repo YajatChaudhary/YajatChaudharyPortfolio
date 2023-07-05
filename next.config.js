74be648493bbe6a6170fdcb930759742c35a71ee
Yajat Chaudhary <chaudharyyajat@gmail.com>
2023-07-05 11:09:50 +0530

    next config file updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    distDir:'dist',
     experimental: {
       fontLoaders: [
         { loader: "@next/font/google", options: { subsets: ["latin"] } },
