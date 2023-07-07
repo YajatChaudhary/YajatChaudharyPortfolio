c8dac43676b8c57ba94f40badc6a38978aac1a44
Yajat Chaudhary <chaudharyyajat@gmail.com>
2023-07-07 16:07:36 +0530

    components added on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client"
 import React from "react";
-import styles from "./home.module.scss"
+import styles from "./home.module.scss";
+import { SideBar } from "@/components/nav/SideBar";
+import { Heading } from "@/components/nav/Heading";
+import { Hero } from "./hero/hero/Hero";
+import { About } from "./about/About";
+import { Experience } from "./experience/Experience";
+import { Contact } from "./contact/Contact";
+
 const Home = () => {
   return (
     <>
       <div className={styles.home}>
+        <SideBar/>
         <main id="main">
+          <Heading/>
+          <Hero/>
+          <About/>
+          <Experience/>
+          <Contact/>
           <div
             style={{
               height: "200px",
@@ -12,7 +26,6 @@ const Home = () => {
                 "linear-gradient(180deg, var(--background), var(--background-dark))",
             }}
           />
-          Hi
         </main>
       </div>
     </>
