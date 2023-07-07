b1472b12ba67f5f0a23cd62103e59fcb324b5175
Yajat Chaudhary <chaudharyyajat@gmail.com>
2023-07-07 15:12:08 +0530

    navbar url updation

--- components/nav/SideBar.tsx
+++ components/nav/SideBar.tsx
@@ -44,7 +44,7 @@ export const SideBar = () => {
           initial={{ x: -70 }}
           animate={{ x: 0 }}
           transition={{ duration: 0.5, delay: 0.1 }}
-          href="/about"
+          href="#about"
           onClick={() => {
             setSelected("about");
           }}
@@ -56,7 +56,7 @@ export const SideBar = () => {
           initial={{ x: -70 }}
           animate={{ x: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
-          href="/projects"
+          href="#projects"
           onClick={() => setSelected("projects")}
           className={selected === "projects" ? styles.selected : ""}
         >
@@ -66,7 +66,7 @@ export const SideBar = () => {
           initial={{ x: -70 }}
           animate={{ x: 0 }}
           transition={{ duration: 0.5, delay: 0.3 }}
-          href="/experience"
+          href="#experience"
           onClick={() => setSelected("experience")}
           className={selected === "experience" ? styles.selected : ""}
         >
@@ -76,7 +76,7 @@ export const SideBar = () => {
           initial={{ x: -70 }}
           animate={{ x: 0 }}
           transition={{ duration: 0.5, delay: 0.4 }}
-          href="/contact"
+          href="#contact"
           onClick={() => setSelected("contact")}
           className={selected === "contact" ? styles.selected : ""}
         >
