7c4b2f0d59aeabf6e2105e0cc5ac4c707eee15af
Yajat Chaudhary <chaudharyyajat@gmail.com>
2023-07-07 15:24:28 +0530

    experience page added

--- app/experience/Experience.tsx
+++ app/experience/Experience.tsx
@@ -1,7 +1,46 @@
-import React from "react";
-import styles from "./experience.module.scss";
-const Experience = () => {
-  return <div className={styles.expereince}>Experience</div>;
+import { SectionHeader } from "@/components/utils/SectionHeader";
+import { ExperienceItem } from "./ExperienceItem";
+
+export const Experience = () => {
+  return (
+    <section className="section-wrapper" id="experience">
+      <SectionHeader title="Experience" dir="l" />
+      {experience.map((item) => (
+        <ExperienceItem key={item.title} {...item} />
+      ))}
+    </section>
+  );
 };
 
-export default Experience;
+const experience = [
+  {
+    title: "HCode Technologies Pvt. Ltd.",
+    position: "Software Engineer Intern",
+    time: "June 2023 - July 2023",
+    location: "Karnal, Haryana",
+    description:
+      "",
+    tech: [
+      "React.js",
+      "Next.js",
+      "Jira",
+      "React Native",
+      "Sass",
+    ],
+  },
+  {
+    title: "Pixonoids",
+    position: "Co-ordinator web",
+    time: "Dec 2023 - April 2023",
+    location: "Hamirpur, Himachal Pradesh",
+    description:
+      "",
+    tech: [
+      "React.js",
+      "Next.js",
+      "Jira",
+      "React Native",
+      "Sass",
+    ],
+  },
+];
