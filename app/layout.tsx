79b22306019d6f165e032c7f2d3b61f8925d4b01
Yajat Chaudhary <chaudharyyajat@gmail.com>
2023-07-08 00:05:36 +0530

    css change -colors,fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Catamaran } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const catamaran = Catamaran({
+  weight: ["200","300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Yajat Chaudhary | Frontend Developer",
@@ -15,9 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={catamaran.className}>{children}</body>
     </html>
   );
 }
